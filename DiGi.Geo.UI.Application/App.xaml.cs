b14f2d2f4432a2b6ff30f14f428b85211cca7044
Jakub Ziolkowski <jakubziolkowski@digiproject.uk>
2024-10-15 00:02:52 +0200

    Initial Implementation

---
+++ DiGi.Geo.UI.Application/App.xaml.cs
@@ -0,0 +1,19 @@
+﻿using DiGi.Geo.UI.Application.Windows;
+using System.Windows;
+
+namespace DiGi.Geo.UI.Application
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : System.Windows.Application
+    {
+        void App_Startup(object sender, StartupEventArgs e)
+        {
+            // Create main application window, starting minimized if specified
+            MainWindow mainWindow = new MainWindow();
+            mainWindow.Show();
+        }
+    }
+
+}
